9ed4c92cb5190252c827f565e98e755f7e150dec
Colin Gorrie <colin@pumpkinseed.io>
2017-01-20 14:42:42 -0500

    Add catchall route (needed for client-side routing)

--- boilerplate/views/app.py
+++ boilerplate/views/app.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template
 app_blueprint = Blueprint("blueprint", __name__)
 
 
-@app_blueprint.route("/")
-def root():
+@app_blueprint.route("/", defaults={"path": ""})
+@app_blueprint.route("/<path:path>")
+def catch_all(path):
     return render_template("app/root.html")
