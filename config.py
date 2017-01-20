dccba1265d6cb7a5323e4dcb1a9adb91f0321956
Colin Gorrie <colin@pumpkinseed.io>
2017-01-20 10:26:39 -0500

    Add config entries for testing using local Mongo

--- config.py
+++ config.py
@@ -6,17 +6,21 @@ class Config(object):
 class ProductionConfig(Config):
     MONGODB_HOST = "wherever.com"
     MONGODB_PORT = 12345
-    MONGODB_DB = "whatever"
+    MONGODB_DB = "boilerplate-prod"
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
     MONGODB_HOST = "localhost"
     MONGODB_PORT = 27017
-    MONGODB_DB = "whatever"
+    MONGODB_DB = "boilerplate-dev"
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
     LIVESERVER_PORT = 8943
+    MONGODB_HOST = "localhost"
+    MONGODB_PORT = 27017
+    MONGODB_DB = "boilerplate-test"
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
