2c998902cd99302d8de9ab88fb27c97a268d3da6
Colin Gorrie <colin@pumpkinseed.io>
2017-01-20 10:25:32 -0500

    Make sure testing works out of the box using nose.

--- boilerplate/__init__.py
+++ boilerplate/__init__.py
@@ -11,20 +11,24 @@ environments = {
 }
 
 
-def create_app(config=None):
-    app = Flask(__name__, instance_relative_config=True)
+def create_app(config=None, instance_relative_config=True):
+    app = Flask(__name__, instance_relative_config=instance_relative_config)
     app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.url_map.strict_slashes = False
 
-    if config is not None:
+    if config in environments:
         app.config.from_object(environments.get(config))
     else:  # default to development config
+        print("No config '{}'. Defaulting to config 'development'.".format(config))
         app.config.from_object("config.DevelopmentConfig")
-    app.config.from_pyfile("config.py")  # instance-based config
 
-    if app.config["MONGODB_HOST"] == "localhost":
-        # no password if we're using localhost
-        del(app.config["MONGODB_USERNAME"])
-        del(app.config["MONGODB_PASSWORD"])
+    if instance_relative_config:
+        app.config.from_pyfile("config.py")  # instance-based config
+
+        if app.config["MONGODB_HOST"] == "localhost":
+            # no password if we're using localhost
+            del(app.config["MONGODB_USERNAME"])
+            del(app.config["MONGODB_PASSWORD"])
 
     db.init_app(app)
 
--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
+import nose
 from flask.ext.script import Manager
 from boilerplate import create_app
 
 
 manager = Manager(create_app)
-manager.add_option("-c", "--config", dest="config", required=False)
+manager.add_option("-c", "--config", dest="config", required=False, default="testing")
+
+
+@manager.command
+def test(only=None):
+    defaultTest = only or "."
+    nose.main(argv=[""], defaultTest=defaultTest)
 
 
 if __name__ == "__main__":
